c5d8369a1a3c330d2f3b30d88aca3c3c1d9e7f7a
Adi Iman <adimixxdev@gmail.com>
2022-10-17 16:32:40 +0800

    WIP

---
+++ Controllers/RolesController.cs
@@ -0,0 +1,25 @@
+﻿using AmonicAirlinesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AmonicAirlinesAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class RolesController : Controller
+    {
+        private AmonicAirlinesContext _context;
+
+        public RolesController(AmonicAirlinesContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Role>>> Index()
+        {
+            var query = _context.Roles;
+            return await query.ToListAsync();
+        }
+    }
+}
